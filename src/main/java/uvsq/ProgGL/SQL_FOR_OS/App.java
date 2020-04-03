4fe1d587851cbd44b0d45cd3229f1d122a54d20b
root <said.mohammed-seghir@ens.uvsq.fr>
2020-04-04 00:02:01 +0200

    Revision du pom.xml

--- src/main/java/uvsq/ProgGL/SQL_FOR_OS/App.java
+++ src/main/java/uvsq/ProgGL/SQL_FOR_OS/App.java
@@ -1,9 +1,10 @@
 package uvsq.ProgGL.SQL_FOR_OS;
 
+
+import org.apache.calcite.adapter.*;
 import org.apache.calcite.sql.SqlNode;
 import org.apache.calcite.sql.parser.SqlParseException;
 import org.apache.calcite.sql.parser.SqlParser;
-import org.apache.calcite.sql.util.SqlVisitor;
 
 
 /**
@@ -14,17 +15,20 @@ public class App
 {
     public static void main( String[] args )
     {
-    	String query = "Select a,b,c from d where d.id in (select id from (select id from e))";
-
+    	String query = "select a,b,c from d where d.id in (select id from (select id from e))";
+    	
 
     	try {
-    	    SqlParser parser = SqlParser.create(query);
+    	   SqlParser parser = SqlParser.create(query);
     	    SqlNode sqlNode = parser.parseQuery();
-
+    	    System.out.println(sqlNode);
     	    //sqlNode.accept(new SqlVisitor());
 
     	} catch (SqlParseException e) {
+    		
     	    e.printStackTrace();
     	}
+    	
+    	//FilesTableFunction table = new FilesTableFunction();
     }
 }
