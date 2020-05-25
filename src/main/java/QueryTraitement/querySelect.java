5c50b3bf811876641673e438e66e762147f01c84
root <said.mohammed-seghir@ens.uvsq.fr>
2020-05-25 23:30:10 +0200

    Remlissage des Méthodes de la classe query

--- src/main/java/QueryTraitement/querySelect.java
+++ src/main/java/QueryTraitement/querySelect.java
@@ -1,41 +1,83 @@
 package QueryTraitement;
 
+
+import org.apache.calcite.sql.SqlSelect;
+
 public class querySelect extends query {
 	
+	private SqlSelect SelectNode;
+	
 	/**
-	  * Exctraire le attributs de la clause Select et les mettre dans queryResult
-	  */
-	 private void ExtractClausesSelect() {
+	 * Exctraire le attributs de la clause Select et les mettre dans queryResult
+	 */
+	
+	public querySelect(final String query) {
+		super(query);
+		setSelectNode(getSelectNode());
 		
-	 }
-	 
-	 /**
-	  * Exctraire le attributs de la clause From et les mettre dans queryResult
-	  */
-	 private void ExtractClausesFrom() {
-		 
-	 }
-	 
-	 /**
-	  * Exctraire le attributs de la clause Where et les mettre dans queryResult
-	  * Types d'attributs de la clause where (type,name,size,contenent,extention, )
-	  */
-	 private void ExtractClausesWhere() {
-		 
-	 }
-	 
-	 /**
-	  *  Exctraire les sattributs de and de la clause Where et les mettre dans queryResult
-	  */
-	 private void ExtractAndFromWhere() {
-		
-	 }
-	 
-	 /**
-	  *  Exctraire les sattributs de OR de la clause Where et les mettre dans queryResult
-	  */
-	 private void ExtractOrFromWhere() {
-		
-	 }
-	 
+	}
+	private void ExtractClausesSelect() {
+
+	}
+
+	/**
+	 * Exctraire le attributs de la clause From et les mettre dans queryResult
+	 */
+	private void ExtractClausesFrom() {
+
+	}
+
+	/**
+	 * Exctraire le attributs de la clause Where et les mettre dans queryResult
+	 * Types d'attributs de la clause where (type,name,size,contenent,extention, )
+	 */
+	private void ExtractClausesWhere() {
+
+	}
+
+	/**
+	 *  Exctraire les sattributs de and de la clause Where et les mettre dans queryResult
+	 */
+	private void ExtractAndFromWhere() {
+
+	}
+
+	/**
+	 *  Exctraire les sattributs de OR de la clause Where et les mettre dans queryResult
+	 */
+	private void ExtractOrFromWhere() {
+
+	}
+
+	/**
+	 * Retourner le premieu noeud de la l'arbre de la requette à parser.
+	 * @return SqlSelect Le premier noeud de la requette de type Select node
+	 */
+	protected SqlSelect getFirstNode() {
+		try {
+			
+			return (SqlSelect)parseQuery();
+		}
+		catch(Exception e) {
+			System.err.println("Impossible de retourner le premier Noeud de la requette type Select");
+			return null;
+		}
+	}
+	
+	/**
+	 * Metttre a jour le Noeud Select
+	 * @param newNode le nouveau noeud select
+	 */
+	public void setSelectNode (SqlSelect newNode) {
+		this.SelectNode = newNode;
+	}
+	
+	/**
+	 * retourner le Noeud Select
+	 * @return Le noeud Select de cette classe
+	 */
+	public SqlSelect getSelectNode () {
+		return this.SelectNode;
+	}
+
 }
